72f8e2ce87ed470c180f28713dc0b81b86c7bc57
Matthew Oliver <protogonoi@gmail.com>
2014-09-02 19:39:29 +1000

    Update VS project to use liblzma by default.

--- SMC/config.h
+++ SMC/config.h
@@ -86,7 +86,7 @@
 /* #undef HAVE_LIBHISTORY */
 
 /* Have compression library */
-/* #undef HAVE_LIBLZMA */
+#define HAVE_LIBLZMA 1
 
 /* Define if pthread library is there (-lpthread) */
 /* #undef HAVE_LIBPTHREAD */
@@ -104,7 +104,7 @@
 #define HAVE_LOCALTIME 1
 
 /* Define to 1 if you have the <lzma.h> header file. */
-/* #undef HAVE_LZMA_H */
+#define HAVE_LZMA_H 1
 
 /* Define to 1 if you have the <malloc.h> header file. */
 #define HAVE_MALLOC_H 1
